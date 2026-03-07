69a9fab6f38ce2d1247d1c36df1f04b102468051
YJE <c0d1ngyje@gmail.com>
2026-03-08 01:12:16 +0900

    Chore: 스프링 프로젝트 초기 구조 설정

---
+++ src/main/java/com/pawpawlog/PawpawlogApplication.java
@@ -0,0 +1,13 @@
+package com.pawpawlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawpawlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawpawlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pawpawlog/PawpawlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawpawlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawpawlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
